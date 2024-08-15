a49351c13f71301a1f2013f03b8c76fa9264406d
MarcoarCarleti <marcarleti@gmail.com>
2024-08-15 19:49:02 -0300

    chore: init migration

---
+++ prisma/migrations/20240815224835_init/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "Lead" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "energyBill" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Lead_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Lead_email_key" ON "Lead"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Lead_cpf_key" ON "Lead"("cpf");
