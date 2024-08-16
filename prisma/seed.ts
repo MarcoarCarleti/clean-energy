331ee9d737ded34e3eb5fa5d7b9287e50d671856
MarcoarCarleti <marcarleti@gmail.com>
2024-08-16 19:21:18 -0300

    chore: add seed script

---
+++ prisma/seed.ts
@@ -0,0 +1,32 @@
+const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcrypt");
+
+const prisma = new PrismaClient();
+
+async function main() {
+  try {
+    const encryptedPassword = await bcrypt.hash("admin", 10);
+    await prisma.user.create({
+      data: {
+        name: "admin",
+        password: encryptedPassword,
+      },
+    });
+
+    console.log("Seed completed successfully");
+  } catch (e: any) {
+    console.error("error seeding database: ", e);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
