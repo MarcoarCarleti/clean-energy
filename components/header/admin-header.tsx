ca607eb5917d2e975ac7ca7f933e35694bed872c
MarcoarCarleti <marcarleti@gmail.com>
2024-08-17 20:51:31 -0300

    style: fix image responsibility

--- components/header/admin-header.tsx
+++ components/header/admin-header.tsx
@@ -13,7 +13,14 @@ export const AdminHeader = () => {
     <div className="flex h-20 bg-foreground z-10 text-background border-b fixed w-screen border-b-zinc-300 justify-between items-center px-8">
       <Link href={"/"} className="flex items-center gap-2">
         <div className="relative w-10 h-10">
-          <Image src={"/logo.png"} alt="logo" fill />
+          <Image
+            src={"/logo.png"}
+            alt="logo"
+            fill
+            style={{
+              objectFit: "cover",
+            }}
+          />
         </div>
         <span className="font-semibold text-3xl">Clean Energy</span>
       </Link>
--- components/header/header.tsx
+++ components/header/header.tsx
@@ -11,18 +11,23 @@ export const Header = () => {
     <div className="flex h-20  z-10 bg-foreground text-background border-b fixed w-screen border-b-zinc-300 justify-between items-center px-8">
       <Link href={"/"} className="flex items-center gap-2">
         <div className="relative w-10 h-10 flex">
-          <Image src={"/logo.png"} alt="logo" fill />
+          <Image
+            src={"/logo.png"}
+            alt="logo"
+            fill
+            style={{
+              objectFit: "cover",
+            }}
+          />
         </div>
         <span className="font-semibold text-3xl">Clean Energy</span>
       </Link>
 
-      <Button
-        variant={"outline"}
-        className="text-white"
-        onClick={() => signIn()}
-      >
-        {status === "authenticated" ? "Painel Admin" : "Login"}
-      </Button>
+      <Link href={"/login"}>
+        <Button variant={"outline"} className="text-white">
+          {status === "authenticated" ? "Painel Admin" : "Login"}
+        </Button>
+      </Link>
     </div>
   );
 };
