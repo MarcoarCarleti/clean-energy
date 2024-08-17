135e9b7b2b5a160783e942d198c922a926269d73
MarcoarCarleti <marcarleti@gmail.com>
2024-08-16 22:01:08 -0300

    style: ensure private layout is responsible

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -4,10 +4,15 @@ import { redirect } from "next/navigation";
 import { Metadata } from "next/types";
 import { headers } from "next/headers";
 import { authOptions } from "@/lib/auth";
+import { AdminHeader } from "@/components/header/admin-header";
 
 interface PriateLayoutProps {
   children: ReactNode;
 }
+
+export const metadata: Metadata = {
+  title: "Clean Energy - Painel Admin",
+};
 
 export default async function PrivateLayout({ children }: PriateLayoutProps) {
   const session = await getServerSession(authOptions);
@@ -17,8 +22,11 @@ export default async function PrivateLayout({ children }: PriateLayoutProps) {
   }
 
   return (
-    <div className="w-screen h-screen">
-      {children}
-    </div>
+    <>
+      <AdminHeader />
+      <div className="py-16 w-screen h-screen px-4 lg:px-0 md:max-h-[100vh] md:overflow-scroll mt-0 lg:max-w-[1200px] lg:mx-auto">
+        {children}
+      </div>
+    </>
   );
 }
